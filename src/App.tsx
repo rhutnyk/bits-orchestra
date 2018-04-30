c148b29d653a194924fe7274df1fc4ad30d2d01d
rhutnyk <roman.hutnyk@bitsorchestra.com>
2018-04-30 17:38:22 +0300

    web hook test

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to BO-React</h1>
         </header>
         <Testimonials/>
       </div>
