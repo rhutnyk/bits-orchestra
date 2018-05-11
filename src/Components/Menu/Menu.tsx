b6ecc046a90f4322668cc48dfeb4f47c1e651450
Vitaliy <webprofit4@gmail.com>
2018-05-11 10:25:57 +0300

    adaptive menu

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -6,31 +6,45 @@ import './Menu.css';
 let clickBtn: boolean = true;
 
 class Menu extends React.Component<any> {
-    
-    addClass(event: any): any {
+
+    public componentWillMount() {
+        if (document.documentElement.clientWidth < 850) {
+            this.addClass();
+        }
+    }
+
+    clickMenu = (): void => {
+        if (document.documentElement.clientWidth < 850) {
+            this.addClass();
+        }
+    }
+
+    addClass(event?: any): any {
         clickBtn = !clickBtn;
         this.forceUpdate();
-        
     }
+
+ 
+
     render() {
         return (
-            
+
             <div>
                 {
-                clickBtn? 
-                <ul className="list-menu">
-                    <li><NavLink exact={true} activeClassName='is-active' to="/">Home</NavLink></li>
-                    <li><NavLink activeClassName='is-active' to="/addTestimonial">Add testimonials</NavLink></li>
-                    <li><NavLink activeClassName='is-active' to="/data">Fire base</NavLink></li>
-                    <li><NavLink activeClassName='is-active' to="/axios">Axios data</NavLink></li>
-                    <li><NavLink activeClassName='is-active' to="/c">Contact</NavLink></li>
-                </ul>
-                :
-                ''
+                    clickBtn ?
+                        <ul onClick={this.clickMenu} className="list-menu">
+                            <li><NavLink exact={true} activeClassName='is-active' to="/">Home</NavLink></li>
+                            <li><NavLink activeClassName='is-active' to="/addTestimonial">Add testimonials</NavLink></li>
+                            <li><NavLink activeClassName='is-active' to="/data">Fire base</NavLink></li>
+                            <li><NavLink activeClassName='is-active' to="/axios">Axios data</NavLink></li>
+                            <li><NavLink activeClassName='is-active' to="/c">Contact</NavLink></li>
+                        </ul>
+                        :
+                        ''
                 }
-               
 
-                <div id="burger-container" className={clickBtn? 'open': ''} onClick={() => {this.addClass(event)}}>
+
+                <div id="burger-container" className={clickBtn ? 'open' : ''} onClick={() => { this.addClass(event) }}>
                     <div id="burger">
                         <span> &nbsp;</span>
                         <span> &nbsp;</span>
